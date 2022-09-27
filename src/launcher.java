33eccc5cf3298201543f8b71ff6e2ca0107cd7c6
louis.bajard <louis.bajard@epita.fr>
2022-09-27 12:44:21 +0200

    Partie 3

--- src/launcher.java
+++ src/launcher.java
@@ -2,13 +2,15 @@ import java.util.Scanner;
 
 class launcher {
     public static void main(String[] args) {
-        System.out.println("Entrez text");
-        Scanner objScan = new Scanner(System.in);
-        String str1 = objScan.nextLine();
-        if (str1.equals("quit") == true) {
 
-        } else {
+        Scanner objScan = new Scanner(System.in);
+        System.out.println("Entrez text");
+        String str1 = objScan.nextLine();
+        while (str1.equals("quit") != true) {
             System.out.println("Unknown command");
+            System.out.println("Entrez text");
+            str1 = objScan.nextLine();
+
         }
 
     }
