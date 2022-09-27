81a85d15a1e11e6ad41a7092e08568d96af03691
louis.bajard <louis.bajard@epita.fr>
2022-09-27 12:47:27 +0200

    Partie 4

--- src/launcher.java
+++ src/launcher.java
@@ -7,11 +7,38 @@ class launcher {
         System.out.println("Entrez text");
         String str1 = objScan.nextLine();
         while (str1.equals("quit") != true) {
-            System.out.println("Unknown command");
-            System.out.println("Entrez text");
+            if (str1.equals("fibo") == true) {
+                fibonachi(args);
+
+            } else {
+                System.out.println("Unknown command");
+
+            }
+            System.out.println("Entrez commande");
             str1 = objScan.nextLine();
 
         }
 
     }
+
+}
+
+public static void fibonachi(String[] args) {
+
+    Scanner objScan = new Scanner(System.in);
+    System.out.println("Entrez nombre");
+    int str1 = objScan.nextInt();
+
+    int f0 = 0;
+    int f1 = 1;
+    int res = 0;
+
+    for (int i = 1; i < str1; i++) {
+        res = f1 + f0;
+        f0 = f1;
+        f1 = res;
+        System.out.println("i: " + i + " f0: " + f0 + " f1: " + f1);
+    }
+    System.out.println("fibo:" + res);
+
 }
